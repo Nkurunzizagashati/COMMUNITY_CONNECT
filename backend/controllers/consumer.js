231431611a3cf618dda369402fd80392eb1b3c5b
Fabrice <f.nkurunziz@alustudent.com>
2024-10-12 12:21:12 +0200

    create a helper for hashing password

--- backend/controllers/consumer.js
+++ backend/controllers/consumer.js
@@ -1,5 +1,6 @@
 import { matchedData, validationResult } from 'express-validator';
 import Consumer from '../models/consumer.js';
+import { hashPassword } from '../utils/helpers.js';
 
 const getAllConsumers = async (req, res) => {
 	try {
@@ -27,6 +28,12 @@ const registerConsumer = async (req, res) => {
 		}
 
 		const data = matchedData(req);
+		const password = data.password;
+
+		const hashedPassword = await hashPassword(password);
+		data.password = hashedPassword;
+
+		// REGISTER CONSUMER
 		const consumer = await Consumer.create(data);
 		const consumerData = consumer.toObject();
 		delete consumerData.password;
@@ -34,7 +41,12 @@ const registerConsumer = async (req, res) => {
 			message: 'Consumer registered successfully',
 			consumerData,
 		});
-	} catch (error) {}
+	} catch (error) {
+		console.log(error.message);
+		return res
+			.status(500)
+			.json({ message: 'Something went wrong' });
+	}
 };
 
 export { getAllConsumers, registerConsumer };
