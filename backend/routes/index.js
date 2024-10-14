05b2f4f6b2419297f916f5c4c1f3662819585146
Fabrice <f.nkurunziz@alustudent.com>
2024-10-14 12:01:00 +0200

    register provider routes in the index.js in the routes folder

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -1,8 +1,10 @@
 import express from 'express';
 import consumerRouter from './consumer.js';
+import providerRouter from './provider.js';
 
 const router = express.Router();
 
 router.use('/consumers', consumerRouter);
+router.use('/providers', providerRouter);
 
 export default router;
