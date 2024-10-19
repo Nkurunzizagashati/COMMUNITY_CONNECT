bacd42c40741088dfcf3d09a9b9a0953f2b4e332
m-umwari <m.umwari@alustudent.com>
2024-10-19 16:54:15 +0200

    updates

--- backend/controllers/provider.js
+++ backend/controllers/provider.js
@@ -1,171 +1,152 @@
-import { matchedData, validationResult } from 'express-validator';
-import { comparePasswords, hashPassword } from '../utils/helpers.js';
+import { matchedData, validationResult } from "express-validator";
+import { comparePasswords, hashPassword } from "../utils/helpers.js";
 import {
-	generateJWTauthToken,
-	generateJWTrefreshToken,
-} from '../utils/authTokens.js';
-import RefreshToken from '../models/token.js';
-import { cloudinaryFileUpload } from '../utils/cloudinary.js';
-import fs from 'fs';
-import Provider from '../models/provider.js';
+  generateJWTauthToken,
+  generateJWTrefreshToken,
+} from "../utils/authTokens.js";
+import RefreshToken from "../models/token.js";
+import { cloudinaryFileUpload } from "../cloudinary.js";
+import fs from "fs";
+import Provider from "../models/provider.js";
 
 const getAllProviders = async (req, res) => {
-	try {
-		const users = await Provider.find();
-		if (users.length === 0) {
-			return res.status(404).json({ message: 'No users found' });
-		}
-		return res.status(200).json({ users });
-	} catch (error) {
-		return res
-			.status(500)
-			.json({ message: 'Something went wrong' });
-	}
+  try {
+    const users = await Provider.find();
+    if (users.length === 0) {
+      return res.status(404).json({ message: "No users found" });
+    }
+    return res.status(200).json({ users });
+  } catch (error) {
+    return res.status(500).json({ message: "Something went wrong" });
+  }
 };
 
 const registerProvider = async (req, res) => {
-	try {
-		const result = validationResult(req);
-		if (!result.isEmpty()) {
-			return res
-				.status(400)
-				.json({ errors: result.array()[0].msg });
-		}
+  try {
+    const result = validationResult(req);
+    if (!result.isEmpty()) {
+      return res.status(400).json({ errors: result.array()[0].msg });
+    }
 
-		const data = matchedData(req);
-		const password = data.password;
+    const data = matchedData(req);
+    const password = data.password;
 
-		// Check for existing email
-		const existingProvider = await Provider.findOne({
-			email: data.email,
-		});
-		if (existingProvider) {
-			return res
-				.status(400)
-				.json({ message: 'Email already registered' });
-		}
+    // Check for existing email
+    const existingProvider = await Provider.findOne({
+      email: data.email,
+    });
+    if (existingProvider) {
+      return res.status(400).json({ message: "Email already registered" });
+    }
 
-		// Check if profile image exists in request
-		if (req.file) {
-			const profileImage = await cloudinaryFileUpload(
-				req.file,
-				'provider_profile_images',
-				res
-			);
-			data.profileImage = profileImage.url;
+    // Check if profile image exists in request
+    if (req.file) {
+      const profileImage = await cloudinaryFileUpload(
+        req.file,
+        "provider_profile_images",
+        res
+      );
+      data.profileImage = profileImage.url;
 
-			// Delete the local file after successful upload
-			fs.unlink(req.file.path, (err) => {
-				if (err) {
-					console.error('Failed to delete local file:', err);
-				}
-			});
-		}
+      // Delete the local file after successful upload
+      fs.unlink(req.file.path, (err) => {
+        if (err) {
+          console.error("Failed to delete local file:", err);
+        }
+      });
+    }
 
-		const hashedPassword = await hashPassword(password);
-		data.password = hashedPassword;
+    const hashedPassword = await hashPassword(password);
+    data.password = hashedPassword;
 
-		// REGISTER PROVIDER
-		const provider = await Provider.create(data);
+    // REGISTER PROVIDER
+    const provider = await Provider.create(data);
 
-		// GENERATE TOKENS
-		const accessToken = generateJWTauthToken({
-			email: provider.email,
-		});
-		const refreshToken = generateJWTrefreshToken({
-			email: provider.email,
-		});
-		const expiresAt = new Date();
-		expiresAt.setDate(expiresAt.getDate() + 7);
+    // GENERATE TOKENS
+    const accessToken = generateJWTauthToken({
+      email: provider.email,
+    });
+    const refreshToken = generateJWTrefreshToken({
+      email: provider.email,
+    });
+    const expiresAt = new Date();
+    expiresAt.setDate(expiresAt.getDate() + 7);
 
-		await RefreshToken.create({
-			token: refreshToken,
-			userId: provider._id,
-			expiresAt,
-		});
+    await RefreshToken.create({
+      token: refreshToken,
+      userId: provider._id,
+      expiresAt,
+    });
 
-		const populatedProvider = await Provider.findById(provider._id)
-			.populate('services')
-			.populate('reviews');
+    const populatedProvider = await Provider.findById(provider._id)
+      .populate("services")
+      .populate("reviews");
 
-		const providerData = populatedProvider.toObject();
-		delete providerData.password;
+    const providerData = populatedProvider.toObject();
+    delete providerData.password;
 
-		res.status(201).json({
-			message: 'Provider registered successfully',
-			accessToken,
-			profileImage: data.profileImage,
-		});
-	} catch (error) {
-		console.error('Error in registration:', error.message);
-		return res
-			.status(500)
-			.json({ message: 'Something went wrong' });
-	}
+    res.status(201).json({
+      message: "Provider registered successfully",
+      accessToken,
+      profileImage: data.profileImage,
+    });
+  } catch (error) {
+    console.error("Error in registration:", error.message);
+    return res.status(500).json({ message: "Something went wrong" });
+  }
 };
 
 const loginProvider = async (req, res) => {
-	console.log(req.body);
-	try {
-		const result = validationResult(req);
-		if (!result.isEmpty()) {
-			return res
-				.status(400)
-				.json({ errors: result.array()[0].msg });
-		}
+  console.log(req.body);
+  try {
+    const result = validationResult(req);
+    if (!result.isEmpty()) {
+      return res.status(400).json({ errors: result.array()[0].msg });
+    }
 
-		const data = matchedData(req);
-		const { email, password } = data;
-		const provider = await Provider.findOne({ email });
-		if (!provider) {
-			return res
-				.status(404)
-				.json({ message: 'Provider not found' });
-		}
+    const data = matchedData(req);
+    const { email, password } = data;
+    const provider = await Provider.findOne({ email });
+    if (!provider) {
+      return res.status(404).json({ message: "Provider not found" });
+    }
 
-		const passwordsMatch = await comparePasswords(
-			password,
-			provider.password
-		);
+    const passwordsMatch = await comparePasswords(password, provider.password);
 
-		if (!passwordsMatch) {
-			return res
-				.status(401)
-				.json({ message: 'Invalid credentials' });
-		}
+    if (!passwordsMatch) {
+      return res.status(401).json({ message: "Invalid credentials" });
+    }
 
-		const accessToken = generateJWTauthToken({
-			email: provider.email,
-		});
-		const refreshToken = generateJWTrefreshToken({
-			email: provider.email,
-		});
-		const expiresAt = new Date();
-		expiresAt.setDate(expiresAt.getDate() + 7);
+    const accessToken = generateJWTauthToken({
+      email: provider.email,
+    });
+    const refreshToken = generateJWTrefreshToken({
+      email: provider.email,
+    });
+    const expiresAt = new Date();
+    expiresAt.setDate(expiresAt.getDate() + 7);
 
-		await RefreshToken.create({
-			token: refreshToken,
-			userId: provider._id,
-			expiresAt,
-		});
+    await RefreshToken.create({
+      token: refreshToken,
+      userId: provider._id,
+      expiresAt,
+    });
 
-		const populatedProvider = await Provider.findById(
-			provider._id
-		).populate('services');
+    const populatedProvider = await Provider.findById(provider._id).populate(
+      "services"
+    );
 
-		const providerData = populatedProvider.toObject();
-		delete providerData.password;
-		res.status(200).json({
-			message: 'Logged in successfully',
-			accessToken,
-			user: providerData,
-		});
-	} catch (error) {
-		console.log(error.message);
-		return res
-			.status(500)
-			.json({ message: 'Something went wrong' });
-	}
+    const providerData = populatedProvider.toObject();
+    delete providerData.password;
+    res.status(200).json({
+      message: "Logged in successfully",
+      accessToken,
+      user: providerData,
+    });
+  } catch (error) {
+    console.log(error.message);
+    return res.status(500).json({ message: "Something went wrong" });
+  }
 };
 
 export { getAllProviders, registerProvider, loginProvider };
--- backend/controllers/service.js
+++ backend/controllers/service.js
@@ -1,133 +1,117 @@
-import jwt from 'jsonwebtoken';
-import { matchedData, validationResult } from 'express-validator';
-import RefreshToken from '../models/token.js';
-import Service from '../models/service.js';
-import { cloudinaryFileUpload } from '../utils/cloudinary.js';
-import fs from 'fs';
-import Provider from '../models/provider.js';
+import jwt from "jsonwebtoken";
+import { matchedData, validationResult } from "express-validator";
+import RefreshToken from "../models/token.js";
+import Service from "../models/service.js";
+import { cloudinaryFileUpload } from "../cloudinary.js";
+import fs from "fs";
+import Provider from "../models/provider.js";
 
 const createService = async (req, res) => {
-	try {
-		const result = validationResult(req);
+  try {
+    const result = validationResult(req);
 
-		if (!result.isEmpty()) {
-			return res
-				.status(400)
-				.json({ errors: result.array()[0].msg });
-		}
+    if (!result.isEmpty()) {
+      return res.status(400).json({ errors: result.array()[0].msg });
+    }
 
-		const data = matchedData(req);
-		const authHeader = req.headers.authorization;
-		let accessToken =
-			authHeader && authHeader.startsWith('Bearer ')
-				? authHeader.split(' ')[1]
-				: null;
+    const data = matchedData(req);
+    const authHeader = req.headers.authorization;
+    let accessToken =
+      authHeader && authHeader.startsWith("Bearer ")
+        ? authHeader.split(" ")[1]
+        : null;
 
-		if (!accessToken) {
-			// If no accessToken is provided, check for refreshToken
-			const refreshToken = req.cookies.refreshToken;
+    if (!accessToken) {
+      // If no accessToken is provided, check for refreshToken
+      const refreshToken = req.cookies.refreshToken;
 
-			if (!refreshToken) {
-				return res.status(401).json({
-					message:
-						'No access token or refresh token provided',
-				});
-			}
+      if (!refreshToken) {
+        return res.status(401).json({
+          message: "No access token or refresh token provided",
+        });
+      }
 
-			// Verify the refresh token
-			try {
-				const decodedRefreshToken = jwt.verify(
-					refreshToken,
-					process.env.REFRESH_TOKEN_SECRET
-				);
+      // Verify the refresh token
+      try {
+        const decodedRefreshToken = jwt.verify(
+          refreshToken,
+          process.env.REFRESH_TOKEN_SECRET
+        );
 
-				// Find the refresh token in the database to ensure it's still valid
-				const storedToken = await RefreshToken.findOne({
-					token: refreshToken,
-				});
-				if (!storedToken) {
-					return res
-						.status(403)
-						.json({ message: 'Not authorized' });
-				}
+        // Find the refresh token in the database to ensure it's still valid
+        const storedToken = await RefreshToken.findOne({
+          token: refreshToken,
+        });
+        if (!storedToken) {
+          return res.status(403).json({ message: "Not authorized" });
+        }
 
-				// Generate a new accessToken
-				accessToken = generateJWTauthToken({
-					email: decodedRefreshToken.email,
-				});
+        // Generate a new accessToken
+        accessToken = generateJWTauthToken({
+          email: decodedRefreshToken.email,
+        });
 
-				res.setHeader('Authorization', `Bearer ${accessToken}`);
-			} catch (err) {
-				return res
-					.status(403)
-					.json({ message: 'Something went wrong' });
-			}
-		}
+        res.setHeader("Authorization", `Bearer ${accessToken}`);
+      } catch (err) {
+        return res.status(403).json({ message: "Something went wrong" });
+      }
+    }
 
-		const isAuthorized = await Provider.findById(data.provider);
+    const isAuthorized = await Provider.findById(data.provider);
 
-		if (!isAuthorized) {
-			return res.status(403).json({ message: 'Not authorized' });
-		}
+    if (!isAuthorized) {
+      return res.status(403).json({ message: "Not authorized" });
+    }
 
-		// Check if images exist in the request
-		if (req.files && req.files.length > 0) {
-			// Array to hold the URLs of uploaded images
-			const images = [];
+    // Check if images exist in the request
+    if (req.files && req.files.length > 0) {
+      // Array to hold the URLs of uploaded images
+      const images = [];
 
-			// Upload each file to Cloudinary and store the URL
-			for (const file of req.files) {
-				const uploadedImage = await cloudinaryFileUpload(
-					file,
-					`${isAuthorized.email}/images`,
-					res
-				);
-				images.push(uploadedImage.url);
+      // Upload each file to Cloudinary and store the URL
+      for (const file of req.files) {
+        const uploadedImage = await cloudinaryFileUpload(
+          file,
+          `${isAuthorized.email}/images`,
+          res
+        );
+        images.push(uploadedImage.url);
 
-				// Delete the local file after successful upload
-				fs.unlink(file.path, (err) => {
-					if (err) {
-						console.error(
-							'Failed to delete local file:',
-							err
-						);
-					}
-				});
-			}
+        // Delete the local file after successful upload
+        fs.unlink(file.path, (err) => {
+          if (err) {
+            console.error("Failed to delete local file:", err);
+          }
+        });
+      }
 
-			// Save the array of image URLs in the data object
-			data.images = images;
-		}
+      // Save the array of image URLs in the data object
+      data.images = images;
+    }
 
-		const createdService = await Service.create(data);
+    const createdService = await Service.create(data);
 
-		res.status(200).json({
-			message: 'Service created successfully',
-			accessToken,
-		});
-	} catch (error) {
-		console.error('Error in creating service:', error.message);
-		return res
-			.status(500)
-			.json({ message: 'Something went wrong' });
-	}
+    res.status(200).json({
+      message: "Service created successfully",
+      accessToken,
+    });
+  } catch (error) {
+    console.error("Error in creating service:", error.message);
+    return res.status(500).json({ message: "Something went wrong" });
+  }
 };
 
 const getAllServices = async (req, res) => {
-	try {
-		const services = await Service.find();
-		if (services.length === 0) {
-			return res
-				.status(404)
-				.json({ message: 'No service found' });
-		}
+  try {
+    const services = await Service.find();
+    if (services.length === 0) {
+      return res.status(404).json({ message: "No service found" });
+    }
 
-		return res.status(200).json({ services });
-	} catch (error) {
-		return res
-			.status(500)
-			.json({ message: 'Something went wrong' });
-	}
+    return res.status(200).json({ services });
+  } catch (error) {
+    return res.status(500).json({ message: "Something went wrong" });
+  }
 };
 
 export { createService, getAllServices };
