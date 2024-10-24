46d44edb688bff5e27bc4e74d1842bee3db8d04c
Fabricegashati <gashati.tech@gmail.com>
2024-10-24 12:58:06 +0200

    responsiveness

--- client/src/config.js
+++ client/src/config.js
@@ -1,6 +1,6 @@
 const variables = {
-	backendUrl2: 'https://isoko-backend.up.railway.app/api/',
-	backendUrl: 'http://localhost:3001/api/',
+	backendUrl: 'https://isoko-backend.up.railway.app/api/',
+	backendUrl2: 'http://localhost:3001/api/',
 };
 
 export default variables;
