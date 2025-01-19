c3a387ee51647b7b698ce6128fe0165072275c4e
Fabrice <f.nkurunziz@alustudent.com>
2025-01-19 14:30:00 +0200

    Fixed bugs

--- client/src/config.js
+++ client/src/config.js
@@ -1,6 +1,7 @@
 const variables = {
 	backendUrl: 'https://isoko-backend.up.railway.app/api/',
 	backendUrl2: 'http://localhost:3001/api/',
+	backendUrl3: 'http://localhost:3001/api/',
 };
 
 export default variables;
