efb22920078c191406b49309ab9ecae787529928
Fabrice <f.nkurunziz@alustudent.com>
2025-01-21 14:24:43 +0200

    update the backend link

--- client/src/config.js
+++ client/src/config.js
@@ -1,5 +1,6 @@
 const variables = {
-	backendUrl: 'https://isoko-backend.onrender.com/api',
+	backendUrl: 'https://isoko-backend.up.railway.app/api',
+	backendUrl4: 'https://isoko-backend.onrender.com/api',
 	backendUrl2: 'http://localhost:3001/api/',
 	backendUrl3: 'http://localhost:3001/api/',
 };
